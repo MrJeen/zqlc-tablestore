932f0b9cb7a0240c821e17535f1250ff46d6861d
d.jy <jy20220520@gmail.com>
2024-01-26 19:11:40 +0800

    优化

--- lib/tablestore.service.ts
+++ lib/tablestore.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { TABLESTORE_OPTIONS } from './tablestore.constant';
 import * as TableStore from 'tablestore';
+import { Int64LE } from 'int64-buffer';
 
 @Injectable()
 export class TableStoreService {
@@ -8,5 +9,32 @@ export class TableStoreService {
 
   constructor(@Inject(TABLESTORE_OPTIONS) options: TableStore.ConfigOptions) {
     this.client = new TableStore.Client(options);
+  }
+
+  async search(options: any) {
+    const data = (await this.client.search(options)) as any;
+    return this.format(data.rows);
+  }
+
+  format(data: any[]) {
+    const result = [];
+    for (const item of data) {
+      const object = {} as any;
+      item.primaryKey.forEach((val: any) => {
+        object[val.name] = this.transform(val.value);
+      });
+      item.attributes.forEach((val: any) => {
+        object[val.columnName] = this.transform(val.columnValue);
+      });
+      result.push(object);
+    }
+    return result;
+  }
+
+  transform(value: any) {
+    if (value instanceof Int64LE) {
+      value = value.toNumber();
+    }
+    return value;
   }
 }
